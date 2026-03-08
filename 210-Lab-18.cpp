e520dc26681837893e41e2d77fe3e57c144dbee1
JoaquinF <joaquinpfelix@gmail.com>
2026-03-08 16:07:04 -0700

    Check 1

--- 210-Lab-18.cpp
+++ 210-Lab-18.cpp
@@ -4,7 +4,25 @@
 #include <string>
 using namespace std;
 
+struct ReviewNode 
+{
+    double rating;
+    string comment;
+    ReviewNode* next;
+};
+
+int getInsertionChoice();
+
 int main()
 {
 
+}
+
+// addAtHead() inserts a new node at beginning
+// arguments: head reference, rating, comment
+//returns: nothing
+void addAtHead(ReviewNode*& head, double rating,
+               const string& comment)
+{
+   ReviewNode* new Node
 }
